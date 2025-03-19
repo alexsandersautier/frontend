e0558290df4d20c29e86b8c7a865f1fc4adb2d2c
Alexander <alexsander@proxsis.net>
2025-03-19 15:52:59 -0300

    fear: catch data in component input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+'use client';
 import Input from "@/components/Input"
 import Button from "@/components/Button"
+import { useState } from "react";
+
 export default function Home() {
+  const [degraus, setDegraus] = useState(0)
   return (
     <main className="flex flex-col items-center justify-center w-full h-screen">
       <div className="padding_5 py-3.5 flex flex-col gap-4 items-center justify-center border rounded-2xl bg-[#010188]">
         <h1 className="font-bold text-white">Conversor de Celsius para Fahrenheit</h1>
-        <Input />
+        <Input degraus={degraus} setDegraus={setDegraus}/>
         <Button />
       </div>
     </main>
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,14 @@
-export default function Input() {
+'use client';
+
+export default function Input({degraus, setDegraus}){
     return(
         <div>
             <input 
                 className="bg-white input_padding outline rounded w-100"
                 type="text" 
                 placeholder="Digite a temperatura em celsius"
+                value={degraus}
+                onChange={(e)=> setDegraus(e.target.value)}
             />
         </div>
     );
